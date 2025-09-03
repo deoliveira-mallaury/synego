21a99b0f5ef065e448f5ebf0ca75ba4f353a9a7e
deoliveira-mallaury <mallaury.de.oliveira.pro@gmail.com>
2025-09-03 14:39:35 +0200

    feat:create project skeleton

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Footer = () => {
+
+  return (
+    <>
+      <footer className="flex w-full flex-wrap justify-center">
+      
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ components/header.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Header = () => {
+
+  return (
+    <>
+      <header className="flex w-full flex-wrap justify-center">
+      
+      </header>
+    </>
+  );
+};
+
+export default Header;
