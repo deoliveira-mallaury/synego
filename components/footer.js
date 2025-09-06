62806d7d2c5abd6cbf3cb4513d7727a2d2ea2366
deoliveira-mallaury <mallaury.de.oliveira.pro@gmail.com>
2025-09-06 10:14:01 +0200

    feat: create footer component

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,10 @@
 import Link from "next/link";
 
 const Footer = () => {
-
   return (
     <>
-      <footer className="flex w-full flex-wrap justify-center">
-      
+      <footer className="fixed bottom-0 w-full flex-wrap justify-center h-1/8 bg-[#0a1d35] p-4 border-t">
+        {/* Ton footer vide ou contenu */}
       </footer>
     </>
   );
